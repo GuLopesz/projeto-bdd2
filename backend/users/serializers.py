1a3467564fba28b6cd1126ac858d624967ecfe33
GuLopesz <gustavo.cl.henrique@gmail.com>
2025-10-12 20:52:26 -0300

    feat: serializers and user views

---
+++ backend/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from users.models import User
+from django.contrib.auth.hashers import make_password
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'user_type', 'date_joined']
+        read_only_fields = ['id', 'user_type', 'date_joined']
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'password_confirm', 'first_name', 'last_name']
+
+    def validate_email(self, value):
+        email_lower = value.lower()
+        if not (email_lower.endswith('@aluno.ifsp.edu.br') or email_lower.endswith('@ifsp.edu.br')):
+            raise serializers.ValidationError('Somente dicentes ou docentes do IFSP são permitidos')
+        return value
+        
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError('As senhas não coincidem.')
+        return data
+        
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+
+        user.save()
+
+        return user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from .models import User
+from .serializers import UserSerializer, UserRegistrationSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return[AllowAny()]
+        return[IsAuthenticated()]
+    
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserRegistrationSerializer
+        return UserSerializer
+    
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(UserSerializer(user).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        user = self.get_object()
+        if request.user.id != user.id and not request.user.is_staff:
+            return Response({'error': 'Você só pode editar seu perfil'}, status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, pk)
