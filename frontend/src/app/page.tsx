dd43102eaefb51fa2fb92e6294eabab9a6137e0d
gudiiiniz <gustavodiniz.ghm@gmail.com>
2025-10-13 17:27:38 -0300

    feat: add questions and subjects cards

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,16 @@
+import { LoginForm } from "@/components/login-form"
+
 export default function Home() {
   return (
     <>
-    <div className="h-30 w-fit text-5xl block mx-auto pt-8">DUVIDAS</div>
+      <div className="h-30 w-fit text-5xl block mx-auto pt-8">Duvida ai</div>
+
+    <div className="flex h-150 w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+
     </>
   );
 }
